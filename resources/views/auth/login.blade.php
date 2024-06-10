8b55ec6337ce39c3f731196a7c8716068a646cb3
Aldo-Aja <revaldoparikesit@gmail.com>
2024-06-10 07:37:18 +0700

    Update:Title Name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#004AAD">
-    <title>E-Presensi Geolocation</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <meta name="description" content="Mobilekit HTML Mobile UI Kit">
     <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
     <link rel="icon" type="image/png" href="{{ asset('icon-512.png') }}" sizes="32x32">
